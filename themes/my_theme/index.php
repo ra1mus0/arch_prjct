3047cab525d7cc93316b013e27e5b5f02a204574
Bober999 <ksandakov00@gmail.com>
2025-11-14 12:02:01 +0700

    index.php with images, fonts and wordpress functions

--- themes/my_theme/index.php
+++ themes/my_theme/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>arch-shop</title>
     <?php wp_head()?>
@@ -12,12 +12,12 @@
             <div class="home-page__inner">
                 <div class="home-page__banner-logo">
                     <div class="banner-logo__img">
-                    <img src="img/ARCHXSTORE.png" alt="logo-banner">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/images/ARCHXSTORE.png" alt="logo-banner">
                     </div>
                 </div>
                 <div class="home-page__banner">
                         <!-- В админке WordPress здесь будет блок изображения -->
-                        <img src="img/banner-available-now.jpg" alt="#"">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/banner-available-now.jpg" alt="#"">
                         <p>Available now.</p>
                 </div>
                 <div class="navbar">
@@ -32,17 +32,17 @@
                     <div class="social-links__list">
                         <a href="#"> 
                             <div class="social-links__item">
-                               <img src="img/tiktok-svgrepo-com 1.svg" alt="">     
+                               <img src="<?php bloginfo('template_url'); ?>/assets/images/tiktok-svgrepo-com 1.svg" alt="">     
                             </div>
                         </a>
                         <a href="#"> 
                             <div class="social-links__item">
-                               <img src="img/telegram-svgrepo-com 2.svg" alt="">     
+                               <img src="<?php bloginfo('template_url'); ?>/assets/images/telegram-svgrepo-com 2.svg" alt="">     
                             </div>
                         </a>
                         <a href="#"> 
                             <div class="social-links__item">
-                                <img src="img/instagram-svgrepo-com 1.svg" alt="">
+                                <img src="<?php bloginfo('template_url'); ?>/assets/images/instagram-svgrepo-com 1.svg" alt="">
                             </div>
                         </a>
                     </div>
@@ -50,7 +50,7 @@
                     <div class="social_links__contact-us">
                         <h3>CONTACT</h3>
                         <p>0/979/066/064</p>
-                        <img src="img/telegram-svgrepo-com 2.svg" alt="">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/telegram-svgrepo-com 2.svg" alt="">
                         <p>@oldiss</p>
                     </div>
             </div>
